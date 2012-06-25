76ff19c179f2d0bcb956d713648a91bf28bece6e
Andreas Schneider <asn@cryptomilk.org>
2012-06-25 16:01:36 +0200

    examples: Fix warnings of allocate_module_test.

--- src/example/allocate_module_test.c
+++ src/example/allocate_module_test.c
@@ -22,22 +22,28 @@ extern void leak_memory();
 extern void buffer_overflow();
 extern void buffer_underflow();
 
-// Test case that fails as leak_memory() leaks a dynamically allocated block.
+/* Test case that fails as leak_memory() leaks a dynamically allocated block. */
 static void leak_memory_test(void **state) {
+    (void) state; /* unused */
+
     leak_memory();
 }
 
-// Test case that fails as buffer_overflow() corrupts an allocated block.
+/* Test case that fails as buffer_overflow() corrupts an allocated block. */
 static void buffer_overflow_test(void **state) {
+    (void) state; /* unused */
+
     buffer_overflow();
 }
 
-// Test case that fails as buffer_underflow() corrupts an allocated block.
+/* Test case that fails as buffer_underflow() corrupts an allocated block. */
 static void buffer_underflow_test(void **state) {
+    (void) state; /* unused */
+
     buffer_underflow();
 }
 
-int main(int argc, char* argv[]) {
+int main(void) {
     const UnitTest tests[] = {
         unit_test(leak_memory_test),
         unit_test(buffer_overflow_test),
