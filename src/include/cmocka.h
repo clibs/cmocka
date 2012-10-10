2ea4503e75660e903c6b5f85677c6500d0bb3e1e
Andreas Schneider <asn@cryptomilk.org>
2012-10-10 10:29:54 +0200

    include: Fix build on OpenIndiana.

--- src/include/cmocka.h
+++ src/include/cmocka.h
@@ -88,14 +88,20 @@ typedef unsigned long long uintptr_t */
 #else /* _WIN32 */
 
 /* ILP32 and LP64 platforms */
-#ifdef __WORDSIZE
+#ifdef __WORDSIZE /* glibc */
 # if __WORDSIZE == 64
 typedef unsigned long int uintptr_t;
 # else
 typedef unsigned int uintptr_t;
 # endif /* __WORDSIZE == 64 */
 #else /* __WORDSIZE */
+
+# if defined(_LP64) || defined(_I32LPx)
 typedef unsigned long int uintptr_t;
+# else
+typedef unsigned int uintptr_t;
+# endif
+
 #endif /* __WORDSIZE */
 
 #endif /* _WIN32 */
