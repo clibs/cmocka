75aa1a468b1552e11cfda35c3af9df2e340a866f
Andreas Schneider <asn@cryptomilk.org>
2018-06-14 12:03:52 +0200

    tests: Give each test a different name

--- tests/test_exception_handler.c
+++ tests/test_exception_handler.c
@@ -19,11 +19,26 @@ static void test_segfault_recovery(void **state)
     s->x = 1;
 }
 
+static void test_segfault_recovery1(void **state)
+{
+    test_segfault_recovery(state);
+}
+
+static void test_segfault_recovery2(void **state)
+{
+    test_segfault_recovery(state);
+}
+
+static void test_segfault_recovery3(void **state)
+{
+    test_segfault_recovery(state);
+}
+
 int main(void) {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(test_segfault_recovery),
-        cmocka_unit_test(test_segfault_recovery),
-        cmocka_unit_test(test_segfault_recovery),
+        cmocka_unit_test(test_segfault_recovery1),
+        cmocka_unit_test(test_segfault_recovery2),
+        cmocka_unit_test(test_segfault_recovery3),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
