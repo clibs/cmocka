c49a7bc032c751095bc8cc24c6e54887c059866a
Andreas Schneider <asn@cryptomilk.org>
2018-08-22 12:52:20 +0200

    cmake: Add new CompilerCheck.cmake

--- DefineOptions.cmake
+++ DefineOptions.cmake
@@ -1,6 +1,7 @@
 option(WITH_STATIC_LIB "Build with a static library" OFF)
 option(WITH_CMOCKERY_SUPPORT "Install a cmockery header" OFF)
 option(UNIT_TESTING "Build with unit testing" OFF)
+option(PICKY_DEVELOPER "Build with picky developer flags" OFF)
 
 if (UNIT_TESTING)
     set(WITH_STATIC_LIB ON)
