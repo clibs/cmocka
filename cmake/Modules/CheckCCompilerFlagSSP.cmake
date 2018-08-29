384f92e1ab34b5fd3a639a7729341a633c85a862
Joel Carlson <joelsoncarl@gmail.com>
2018-08-29 11:46:18 -0600

    cmake: Fix SSP compiler flag check
    Need to use CMAKE_REQUIRED_FLAGS when checking SSP, not
    CMAKE_REQUIRED_DEFINITIONS.
    
    Signed-off-by: Joel Carlson <JoelsonCarl@gmail.com>
    Reviewed-by: Andreas Schneider <asn@cryptomilk.org>

--- cmake/Modules/CheckCCompilerFlagSSP.cmake
+++ cmake/Modules/CheckCCompilerFlagSSP.cmake
@@ -20,10 +20,10 @@
 include(CheckCSourceCompiles)
 
 macro(CHECK_C_COMPILER_FLAG_SSP _FLAG _RESULT)
-   set(SAFE_CMAKE_REQUIRED_DEFINITIONS "${CMAKE_REQUIRED_DEFINITIONS}")
-   set(CMAKE_REQUIRED_DEFINITIONS "${_FLAG}")
+   set(SAFE_CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS}")
+   set(CMAKE_REQUIRED_FLAGS "${_FLAG}")
 
    check_c_source_compiles("int main(int argc, char **argv) { char buffer[256]; return buffer[argc]=0;}" ${_RESULT})
 
-   set(CMAKE_REQUIRED_DEFINITIONS "${SAFE_CMAKE_REQUIRED_DEFINITIONS}")
+   set(CMAKE_REQUIRED_FLAGS "${SAFE_CMAKE_REQUIRED_FLAGS}")
 endmacro(CHECK_C_COMPILER_FLAG_SSP)
