b12767a8bb3b87cc89e871d73d65291883df1270
Jan Kundrát <jan.kundrat@cesnet.cz>
2018-09-11 01:25:12 +0200

    cmake: Do not use uninitialized variables
    These variables were used without initialization, which means that the
    pkg-config files were being put into /pkgconfig, etc.
    
    Fixes: https://travis-ci.org/sysrepo/sysrepo/builds/426872891
    
    Signed-off-by: Jan Kundrát <jan.kundrat@cesnet.cz>
    Reviewed-by: Andreas Schneider <asn@cryptomilk.org>

--- cmocka.pc.cmake
+++ cmocka.pc.cmake
@@ -1,5 +1,5 @@
 Name: ${PROJECT_NAME}
 Description: The cmocka unit testing library
 Version: ${PROJECT_VERSION}
-Libs: -L${LIB_INSTALL_DIR} -lcmocka
-Cflags: -I${INCLUDE_INSTALL_DIR}
+Libs: -L${CMAKE_INSTALL_FULL_LIBDIR} -lcmocka
+Cflags: -I${CMAKE_INSTALL_FULL_INCLUDEDIR}
