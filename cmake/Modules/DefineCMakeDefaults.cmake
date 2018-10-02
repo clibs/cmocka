17e3b51db1a53416ddf5193beee7c7f17d1629d4
Andreas Schneider <asn@cryptomilk.org>
2018-10-02 14:45:09 +0200

    cmake: Always build position independent code
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>

--- cmake/Modules/DefineCMakeDefaults.cmake
+++ cmake/Modules/DefineCMakeDefaults.cmake
@@ -16,3 +16,6 @@ set(CMAKE_COLOR_MAKEFILE ON)
 
 # Create the compile command database for clang by default
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+# Always build with -fPIC
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
