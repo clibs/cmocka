22264569de1a894991b8876caf420b09bab1aa46
Andreas Schneider <asn@cryptomilk.org>
2019-11-19 15:57:27 +0100

    cmake: Add options for BUILD_SHARED_LIBS

--- DefineOptions.cmake
+++ DefineOptions.cmake
@@ -1,11 +1,5 @@
-option(WITH_STATIC_LIB "Build with a static library" OFF)
+option(BUILD_SHARED_LIBS "Build shared libraries" ON)
 option(WITH_CMOCKERY_SUPPORT "Install a cmockery header" OFF)
 option(WITH_EXAMPLES "Build examples" ON)
 option(UNIT_TESTING "Build with unit testing" OFF)
 option(PICKY_DEVELOPER "Build with picky developer flags" OFF)
-
-if (WITH_STATIC_LIB)
-    set(BUILD_SHARED_LIBS OFF)
-else (WITH_STATIC_LIB)
-    set(BUILD_SHARED_LIBS ON)
-endif (WITH_STATIC_LIB)
