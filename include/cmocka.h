clibs/cmocka

commit 8d99418ef64c36d89c126ccf3f5e033a204e60d4

include: Change from should to must and add stdint.h
Fixes #15

Signed-off-by: Andreas Schneider <asn@cryptomilk.org>

include/cmocka.h
@@ -41,12 +41,13 @@ int __stdcall IsDebuggerPresent();
 /**
  * @defgroup cmocka The CMocka API
  *
- * These headers or their equivalents should be included prior to including
+ * These headers or their equivalents MUST be included prior to including
  * this header file.
  * @code
  * #include <stdarg.h>
  * #include <stddef.h>
  * #include <setjmp.h>
+ * #include <stdint.h>
  * @endcode
  *
  * This allows test applications to use custom definitions of C standard

commit 4b8c67c3fb3be78ad1a37e3253e3776b0cd611d9

doc: Remove redundant include line in documentation
stdint was added to inline doc in 2019 and to main doc in 2020, but with
that accidentally to the header file again.

Fixes: 8d99418ef64c ("include: Change from should to must and add stdint.h")
Fixes: e31967fbedc7 ("doc: Add stdint.h to headers")
Signed-off-by: Alexander Dahl <ada@thorsis.com>
Reviewed-by: Andreas Schneider <asn@cryptomilk.org>

include/cmocka.h
@@ -48,7 +48,6 @@ int __stdcall IsDebuggerPresent();
  * #include <stddef.h>
  * #include <stdint.h>
  * #include <setjmp.h>
- * #include <stdint.h>
  * @endcode
  *
  * This allows test applications to use custom definitions of C standard