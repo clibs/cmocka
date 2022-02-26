clibs/cmocka

commit 400038f6cac93f9423dc3e8430e3029499b19061

include: Add a group for mock objects.

include/cmocka.h
@@ -123,6 +123,18 @@ typedef unsigned int uintptr_t;
 #define cast_ptr_to_largest_integral_type(value) \
 cast_to_largest_integral_type(cast_to_pointer_integral_type(value))
 
+/**
+ * @defgroup cmocka_mock Mock Objects
+ * @ingroup cmocka.
+ *
+ * Mock objects mock objects are simulated objects that mimic the behavior of
+ * real objects. Instead of calling the real objects, the tested object calls a
+ * mock object that merely asserts that the correct methods were called, with
+ * the expected parameters, in the correct order.
+ *
+ * @{
+ */
+
 #ifdef DOXYGEN
 /**
  * @brief Retrieve a return value of the current function.
@@ -188,6 +200,8 @@ void will_return_count(#function, void *value, int count);
     _will_return(#function, __FILE__, __LINE__, \
                  cast_to_largest_integral_type(value), count)
 #endif
+
+/** @} */
 
 /*
  * Add a custom parameter checking function.  If the event parameter is NULL

commit 629ae858daa36f3523bce29b482cdbaec48bbb28

doc: Remove redundant words
Fixes: 400038f6cac9 ("include: Add a group for mock objects.")
Signed-off-by: Alexander Dahl <ada@thorsis.com>
Reviewed-by: Andreas Schneider <asn@cryptomilk.org>

include/cmocka.h
@@ -100,7 +100,7 @@ int __stdcall IsDebuggerPresent();
  * @defgroup cmocka_mock Mock Objects
  * @ingroup cmocka
  *
- * Mock objects mock objects are simulated objects that mimic the behavior of
+ * Mock objects are simulated objects that mimic the behavior of
  * real objects. Instead of calling the real objects, the tested object calls a
  * mock object that merely asserts that the correct methods were called, with
  * the expected parameters, in the correct order.