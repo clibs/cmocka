1759ec25c396d85a172a2424af027098395298ce
Andreas Schneider <asn@cryptomilk.org>
2022-03-12 18:05:58 +0100

    cmocka: Add assert_uint_equal()

--- include/cmocka.h
+++ include/cmocka.h
@@ -1193,6 +1193,25 @@ void assert_int_equal(intmax_t a, intmax_t b);
 
 #ifdef DOXYGEN
 /**
+ * @brief Assert that the two given unsinged integers are equal.
+ *
+ * The function prints an error message to standard error and terminates the
+ * test by calling fail() if the integers are not equal.
+ *
+ * @param[in]  a  The first unsigned integer to compare.
+ *
+ * @param[in]  b  The unsigned integer to compare against the first one.
+ */
+void assert_uint_equal(int a, int b);
+#else
+#define assert_uint_equal(a, b) \
+    _assert_uint_equal(cast_to_uintmax_type(a), \
+                       cast_to_uintmax_type(b), \
+                      __FILE__, __LINE__)
+#endif
+
+#ifdef DOXYGEN
+/**
  * @brief Assert that the two given integers are not equal.
  *
  * The function prints an error message to standard error and terminates the
@@ -2291,6 +2310,10 @@ void _assert_int_not_equal(const intmax_t a,
                            const intmax_t b,
                            const char * const file,
                            const int line);
+void _assert_uint_equal(const uintmax_t a,
+                        const uintmax_t b,
+                        const char * const file,
+                        const int line);
 void _assert_string_equal(const char * const a, const char * const b,
                           const char * const file, const int line);
 void _assert_string_not_equal(const char * const a, const char * const b,
