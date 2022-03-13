0b5e7d2f1c9c99be07f40fde95a134f1ea34f0b1
Andreas Schneider <asn@cryptomilk.org>
2022-03-13 09:09:15 +0100

    tests: Add negative test for assert_uint_not_equal()

--- tests/test_assert_u_int_fail.c
+++ tests/test_assert_u_int_fail.c
@@ -33,11 +33,20 @@ static void test_assert_uint_equal_fail(void **state)
     assert_uint_equal(0, UINTMAX_MAX);
 }
 
+static void test_assert_uint_not_equal_fail(void **state)
+{
+    (void)state; /* unused */
+    assert_uint_not_equal(0, 0);
+    assert_uint_not_equal(1, 1);
+    assert_uint_not_equal(UINTMAX_MAX, UINTMAX_MAX);
+}
+
 int main(void) {
     const struct CMUnitTest integer_tests[] = {
         cmocka_unit_test(test_assert_int_equal_fail),
         cmocka_unit_test(test_assert_int_not_equal_fail),
         cmocka_unit_test(test_assert_uint_equal_fail),
+        cmocka_unit_test(test_assert_uint_not_equal_fail),
     };
 
     return cmocka_run_group_tests(integer_tests, NULL, NULL);
