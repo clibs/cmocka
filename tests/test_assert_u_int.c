b7370ada1fa4ed2673b594849eddacbf54c05997
Andreas Schneider <asn@cryptomilk.org>
2022-03-13 08:45:43 +0100

    tests: Add test for assert_uint_equal()

--- tests/test_assert_u_int.c
+++ tests/test_assert_u_int.c
@@ -25,10 +25,19 @@ static void test_assert_int_not_equal(void **state)
     assert_int_not_equal(INTMAX_MAX, INTMAX_MIN);
 }
 
+static void test_assert_uint_equal(void **state)
+{
+    (void)state; /* unused */
+    assert_uint_equal(0, 0);
+    assert_uint_equal(1, 1);
+    assert_uint_equal(UINTMAX_MAX, UINTMAX_MAX);
+}
+
 int main(void) {
     const struct CMUnitTest integer_tests[] = {
         cmocka_unit_test(test_assert_int_equal),
         cmocka_unit_test(test_assert_int_not_equal),
+        cmocka_unit_test(test_assert_uint_equal),
     };
 
     return cmocka_run_group_tests(integer_tests, NULL, NULL);
