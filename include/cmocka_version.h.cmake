eb554f3384fc53b18c85470a0a5e60e8f4b7fe25
Andreas Schneider <asn@cryptomilk.org>
2022-08-08 09:52:39 +0200

    include: Add cmocka_version.h

---
+++ include/cmocka_version.h.cmake
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2022      Andreas Schneider <asn@cryptomilk.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _CMOCKA_VERSION_H
+#define _CMOCKA_VERSION_H
+
+/* cmocka version macros */
+#define CMOCKA_VERSION_INT(a, b, c) ((a) << 16 | (b) << 8 | (c))
+#define CMOCKA_VERSION_DOT(a, b, c) a ##.## b ##.## c
+#define CMOCKA_VERSION(a, b, c) CMOCKA_VERSION_DOT(a, b, c)
+
+/* cmocka version */
+#define CMOCKA_VERSION_MAJOR  @cmocka_VERSION_MAJOR@
+#define CMOCKA_VERSION_MINOR  @cmocka_VERSION_MINOR@
+#define CMOCKA_VERSION_MICRO  @cmocka_VERSION_PATCH@
+
+#define CMOCKA_VERSION_INT CMOCKA_VERSION_INT(CMOCKA_VERSION_MAJOR, \
+                                           CMOCKA_VERSION_MINOR, \
+                                           CMOCKA_VERSION_MICRO)
+#define CMOCKA_VERSION     CMOCKA_VERSION(CMOCKA_VERSION_MAJOR, \
+                                       CMOCKA_VERSION_MINOR, \
+                                       CMOCKA_VERSION_MICRO)
+
+#endif /* _CMOCKA_VERSION_H */
